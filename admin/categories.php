31fd6b103d1ef5f82669919b250cd417bbbefec9
Robin Vivien <robin.vivien@bnc.ca>
2020-05-30 09:59:09 -0400

    change fies

---
+++ admin/categories.php
@@ -0,0 +1,70 @@
+<?php include "includes/admin_header.php"?>
+    <div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome to admin
+                        <small>Author</small>
+                    </h1>
+                    
+                    <div class="col-xs-6">
+                        <form action="">
+                            <div class="form-group">
+                               <label for="cat-title">Add category</label>
+                                <input class="form-control" type="text" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+                        </form>
+                    </div>
+                    
+                    
+                    <div class="col-xs-6">
+                        
+                        <?php
+                            $query = "SELECT * FROM categories";
+                            $select_categories = mysqli_query($connection, $query);
+                        ?>
+                        
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                               <?php
+                                    while($row = mysqli_fetch_assoc($select_categories)){
+                                        $cat_title = $row["cat_title"];
+                                        $cat_id = $row["cat_id"];
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $cat_id?></td>
+                                            <td><?php echo $cat_title?></td>
+                                        </tr>
+                                <?php   }?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php"?>
