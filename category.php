b6ecd34043fe9375820d6ea97f18673279994a9e
Robin Vivien <robin.vivien@bnc.ca>
2020-07-19 16:11:28 -0400

    save progress

--- category.php
+++ category.php
@@ -15,7 +15,10 @@
                 <small>Secondary Text</small>
              </h1>
             <?php
-                $query = "SELECT * FROM posts";
+                if(isset($_GET['category'])){
+                    $post_category = $_GET['category'];
+                }
+                $query = "SELECT * FROM posts WHERE post_category_id = '{$post_category}'";
                 $select_all_posts_query = mysqli_query($connection, $query);
 
                 while($row = mysqli_fetch_assoc($select_all_posts_query)){
